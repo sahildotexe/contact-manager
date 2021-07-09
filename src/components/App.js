5a907012850acfecb4dd7f927ccc90b2dcc47b23
Sahil Kaling <sahil@example.com>
2021-07-09 19:13:52 +0530

    CRUD implemented

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
+import EditContact from './EditContact';
+import api from "../api/contacts";
 import {uuid} from 'uuidv4';
 import "./App.css";
 import ContactDetails from './ContactDetails';
@@ -13,13 +15,36 @@ function App() {
 
   const [contacts,setContacts] = useState([]);
 
-  const addContactHandler = (contact) => {
-    console.log(contact);
-
-    setContacts([...contacts,{id: uuid(), ...contact}]);
+  //Retrieve COntacts
+  const retrieveContacts = async () => {
+     const response = await api.get("/contacts");
+     return response.data;
   }
 
-  const removeContactHandler = (id) => {
+  const addContactHandler = async (contact) => {
+    console.log(contact);
+    const request = {
+      id: uuid(),
+      ...contact
+    }
+
+    const response = await api.post("/contacts",request)
+
+    setContacts([...contacts,response.data]);
+  }
+
+  
+  const updateContactHandler = async  (contact) => {
+     const response = await api.put(`/contacts/${contact.id}`,contact);
+     const {id,name,email} =  response.data;
+     setContacts(contacts.map(contact => {
+       return contact.id === id ? {...response.data} : contact;
+     }))
+
+  }
+
+  const removeContactHandler =  async (id) => {
+    await api.delete(`/contacts/${id}`);
      const newContactList = contacts.filter((contact) => {
         return contact.id != id;
      });
@@ -30,8 +55,17 @@ function App() {
 
 
   useEffect(() => {
-    const retrieveContacts = JSON.parse( localStorage.getItem(LOCAL_STORAGE_KEY));
-    if(retrieveContacts) setContacts(retrieveContacts);
+    // const retrieveContacts = JSON.parse( localStorage.getItem(LOCAL_STORAGE_KEY));
+    // if(retrieveContacts) setContacts(retrieveContacts);
+    
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if(allContacts) setContacts(allContacts);
+    };
+
+    getAllContacts();
+
+
     },[]);
 
   useEffect(() => {
@@ -59,6 +93,13 @@ function App() {
       
       render = {(props) => (
         <AddContact {...props} addContactHandler={addContactHandler}/>
+      )}
+      />
+
+<Route path= "/edit" exact
+      
+      render = {(props) => (
+        <EditContact {...props} updateContactHandler={updateContactHandler}/>
       )}
       />
       
